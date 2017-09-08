793578cf294dc24da2015702d4ad29abe4a0ad02
Maciek Abramowski <m.abramowski@wp.pl>
2017-09-08 16:23:51 +0200

    shared state of modules

---
+++ bucky.js
@@ -0,0 +1,5 @@
+var movies = require('./movies');
+
+
+
+console.log("Bucky's favorite movie is:" + movies.favMovie);
---
+++ emily.js
@@ -0,0 +1,5 @@
+var movies = require('./movies');
+
+movies.favMovie = "The Notebook";
+
+console.log("Emily's favorite movie is:" + movies.favMovie);
